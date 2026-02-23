320dc203f8d606ad27daf06bb82f617ba620a1a7
ysuovnii <ysuovnii@local>
2026-02-23 23:38:07 +0530

    hide key

--- service/auth.js
+++ service/auth.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken')
-const sec = "random@$#123&^"
+require('dotenv').config()
+const KEY = process.env.JWT_KEY
 
 // this will create tokens
 function setUser(user) {
     const payload = {
         ...user 
     }
-    return jwt.sign(payload, sec)
+    return jwt.sign(payload, KEY)
 }
 
 function getUser(id) {
